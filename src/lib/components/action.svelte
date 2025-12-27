909eb5ba07390b4f42f70194e32f7f55d132721b
Nnadi Victory <chikamagozi25@gmail.com>
2025-12-27 14:14:31 +0100

    complete action component
    Just finished implementing the delete functionality

--- src/lib/components/action.svelte
+++ src/lib/components/action.svelte
@@ -1,21 +1,40 @@
 <script lang="ts">
 	import * as DropdownMenu from '$lib/components/ui/dropdown-menu/index.js';
-	import { Ellipsis, Pencil } from '@lucide/svelte';
+	import * as AlertDialog from '$lib/components/ui/alert-dialog/index.js';
+	import { Ellipsis, Pencil, Trash } from '@lucide/svelte';
 	import type { Playlist } from '$lib/server/db/schema';
 	import { authClient } from '$lib/auth-client';
 	import { usePlaylistModal } from '$lib/hooks/usePlaylistModal.svelte';
+	import { buttonVariants } from './ui/button/index.js';
+	import { Spinner } from './ui/spinner/index.js';
+	import { cn } from '../utils';
+	import { useDeletePlaylist } from '$lib/hooks/useDeletePlaylist';
+	import { toast } from 'svelte-sonner';
 
 	const session = authClient.useSession();
 
 	let { playlist } = $props<{ playlist: Playlist }>();
 
 	const playlistModal = usePlaylistModal();
+	const { mutateAsync: deletePlaylist, isPending: isDeleting } = useDeletePlaylist();
 
 	const isUserPlaylist = $derived($session.data?.user?.id === playlist.userId);
+
+	let actionModalOpen = $state(false);
+
+	async function handleDelete() {
+		try {
+			await deletePlaylist(playlist.id);
+			actionModalOpen = false;
+			toast.success('Playlist deleted');
+		} catch (error) {
+			toast.error((error as Error).message);
+		}
+	}
 </script>
 
 {#if isUserPlaylist}
-	<DropdownMenu.Root>
+	<DropdownMenu.Root bind:open={actionModalOpen}>
 		<DropdownMenu.Trigger class="action-trigger-btn">
 			<Ellipsis size={16} strokeWidth={2} class="cursor-pointer text-primary" />
 		</DropdownMenu.Trigger>
@@ -27,6 +46,36 @@
 				<Pencil size={16} strokeWidth={2} class="text-primary" />
 				<p>Edit</p>
 			</DropdownMenu.Item>
+			<AlertDialog.Root onOpenChange={(open) => !open && (actionModalOpen = false)}>
+				<AlertDialog.Trigger
+					class="group flex w-full items-center gap-2 rounded-md px-2 py-1 text-red-500! transition-all hover:bg-red-500 hover:text-white!"
+				>
+					<Trash size={16} strokeWidth={2} class="text-red-500! group-hover:text-white!" />
+					<p>Delete</p>
+				</AlertDialog.Trigger>
+				<AlertDialog.Content>
+					<AlertDialog.Header>
+						<AlertDialog.Title>Are you absolutely sure?</AlertDialog.Title>
+						<AlertDialog.Description>
+							This action cannot be undone. This will permanently delete this playlist.
+						</AlertDialog.Description>
+					</AlertDialog.Header>
+					<AlertDialog.Footer>
+						<AlertDialog.Cancel>Cancel</AlertDialog.Cancel>
+						<AlertDialog.Action
+							class={cn(buttonVariants({ variant: 'destructive' }))}
+							onclick={handleDelete}
+							disabled={isDeleting}
+						>
+							{#if isDeleting}
+								<Spinner size="sm" /> Deleting
+							{:else}
+								Delete
+							{/if}
+						</AlertDialog.Action>
+					</AlertDialog.Footer>
+				</AlertDialog.Content>
+			</AlertDialog.Root>
 		</DropdownMenu.Content>
 	</DropdownMenu.Root>
 {/if}
