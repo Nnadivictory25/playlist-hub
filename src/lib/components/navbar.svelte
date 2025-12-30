5ddc9df971c1db9011442aabff95e1a70632011e
Nnadi Victory <chikamagozi25@gmail.com>
2025-12-30 15:53:25 +0100

    optimize images in home + make navbar link accesible

--- src/lib/components/navbar.svelte
+++ src/lib/components/navbar.svelte
@@ -7,9 +7,18 @@
 <nav
 	class="fixed top-10 left-1/2 z-50 mx-auto flex w-[90%] max-w-[550px] -translate-x-1/2 items-center justify-between rounded-full border border-gray-300/30 px-8 py-3 backdrop-blur-lg"
 >
-	<a href="/" class="flex items-center gap-1">
-		<div class="flex size-9 items-center justify-center rounded-md bg-primary text-white">
-			<Music size={25} />
+	<a
+		href="/"
+		class="flex items-center gap-1"
+		aria-label="PlaylistHub Home"
+		title="PlaylistHub Home"
+	>
+		<div
+			class="flex size-9 items-center justify-center rounded-md bg-primary text-white"
+			role="img"
+			aria-label="PlaylistHub logo"
+		>
+			<Music size={25} aria-hidden="true" focusable="false" />
 		</div>
 		<span class="hidden text-lg font-medium lg:block">PlaylistHub</span>
 	</a>
